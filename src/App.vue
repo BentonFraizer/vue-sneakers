8ddf5bb026271041493a8363c0a33846b70bda23
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-10-09 23:17:28 +0300

    feat(my-hello): counter by options API

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+ import MyHello from "@/components/MyHello.vue";
 </script>
 
 <template>
+  <MyHello />
 </template>
 
 <style scoped>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Count: {{ count }}</h1>
+  <button @click="increment">+1</button>
+</template>
+
+
+<style scoped></style>
