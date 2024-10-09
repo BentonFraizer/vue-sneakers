78f5453fbdde7ba43ee5ae711d561dff6f9e3f04
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-10-10 01:22:43 +0300

    feat(my-hello): slot understanding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
- import MyHello from "@/components/MyHello.vue";
+import MyHello from "@/components/MyHello.vue";
 </script>
 
 <template>
-  <MyHello title="Кроссовки Puma" :price="19500" is-added />
-  <MyHello isAdded />
+  <MyHello title="Кроссовки Puma" :price="19500" is-added>
+    <button>Добавить</button>
+  </MyHello>
+  <MyHello isAdded>
+    <h5>Нет в наличии</h5>
+  </MyHello>
+  <MyHello isAdded>
+    <MyHello>
+      <h5>Нет в наличии</h5>
+    </MyHello>
+  </MyHello>
   <MyHello :isAdded="false"/>
 </template>
 
--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -11,11 +11,15 @@ defineProps({
     <h2>Товар: {{ title }}</h2>
     <h3>Цена: {{ price }}р.</h3>
     <h3>Добавлен в корзину: {{ isAdded ? "Да" : "Нет" }}</h3>
+    <slot/>  <!-- slot => аналог children из React  -->
   </div>
+
 </template>
 
 <style scoped>
 .product {
+  display: flex;
+  flex-direction: column;
   border: 1px solid blue;
   padding: 10px;
   margin-bottom: 10px;
