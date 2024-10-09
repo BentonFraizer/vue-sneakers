d81be434f189b763c8179d9a9ef8623685398cfb
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-10-10 00:23:17 +0300

    feat(my-hello): counter by compositions API

--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,21 +1,30 @@
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    }
-  }
+<!-- Composition API -->
+<script setup>
+import {ref} from "vue"
+
+const count = ref(0);
+const text = ref(0);
+
+const increment = () => {
+  count.value++;
+}
+
+const updateCount = (e) => {
+  count.value = Number(e.target.value);
 }
 </script>
 
 <template>
   <h1>Count: {{ count }}</h1>
   <button @click="increment">+1</button>
+  <!--  Плохой пример кода -->
+  <input @input="updateCount">
+  <br>
+  <!-- Хороший пример. Из документации  -->
+  <h1>Text: {{ text }}</h1>
+  <input
+      :value="text"
+      @input="event => text = event.target.value">
 </template>
 
 
